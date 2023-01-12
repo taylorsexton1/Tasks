6b134a56165e2b8e5f148988c880e3e1e20952c0
taylorsexton1 <trs0049@mix.wvu.edu>
2023-01-12 11:12:48 -0500

    Taylor Sexton Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,1 +1,28 @@
 # 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
+library(swirl)
+swirl()
+taylorsexton
+1
+1
+1
+5+7
+x <- 5+7
+x
+y<- x-3
+y
+z <- c(1.1,9,3.14)
+?c
+z
+c(z,555,z)
+z*2+100
+my_sqrt <-sqrt(z-1)
+1
+my_sqrt
+my_div <- z/my_sqrt
+1
+my_div
+c(1,2,3,4)+c(0,10)
+c(1,2,3,4)+c(0,10,100)
+z*2+1000
+my_div
+No
